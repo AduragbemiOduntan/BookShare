0d3c6a4e6a529026dd0b0f85020019b659bb3e5d
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-07-03 17:36:20 +0100

    FIRST INFRAS

---
+++ BookShare.Persistence/Common/RepositoryBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShare.Persistence.Common
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+        public void Create(T entity) =>  RepositoryContext.Set<T>().Add(entity);
+        
+
+        public void Delete(T entity) => RepositoryContext.Set<T>().Remove(entity);
+       
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            return !trackChanges ?
+                 RepositoryContext.Set<T>()
+                 .AsNoTracking() :
+                 RepositoryContext.Set<T>();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            return !trackChanges ?
+                RepositoryContext.Set<T>()
+                .Where(expression)
+                .AsNoTracking() :
+                RepositoryContext.Set<T>()
+                .Where(expression);
+
+        }
+
+        public void Update(T entity)
+        {
+            RepositoryContext.Set<T>().Update(entity);
+        }
+    {
+
+
+    }
+}
