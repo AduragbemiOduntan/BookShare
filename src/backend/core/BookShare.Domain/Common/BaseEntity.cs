f64650defab02a6d81c0a64ca5d55b2f19d726ed
Aduragbemi Oduntan <aduragbemioa@gmail.com>
2023-07-03 10:04:35 +0100

    Add project files.

---
+++ src/backend/core/BookShare.Domain/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShare.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.UtcNow;
+        public string? ModifiedBy { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
