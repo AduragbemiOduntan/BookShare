7617708b39ba8cdad53c4f57ebcc45420566bb5c
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-07-03 15:17:16 +0100

    FirstModelChanges

---
+++ src/backend/core/BookShare.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using BookShare.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShare.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public int CategoryId { get; set; }
+        public string? Name{ get; set; }
+    }
+}
