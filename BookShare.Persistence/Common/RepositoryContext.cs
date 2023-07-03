0e81c9947a88f641d246649724b881c52f5da276
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-07-03 20:38:03 +0100

    w

---
+++ BookShare.Persistence/Common/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShare.Persistence.Common
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextObtions obtions) : base(obtions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            public DbSet<Book> books { get; set; }
+
+    }
+}
---
+++ BookShare.Persistence/Common/RepositoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShare.Persistence.Common
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _repositoryContext;
+        private readonly Lazy<IBookRepository>
+
+    }
+}
